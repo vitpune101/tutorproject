8eabc118a301ecfb4a89a89f9546ba12a71bb18b
Varad <varad2512@gmail.com>
2015-09-25 23:26:33 +0530

    Made some changes folks!

--- getatutor/src/main/java/org/getatutor/com/getatutor/MyResource.java
+++ getatutor/src/main/java/org/getatutor/com/getatutor/MyResource.java
@@ -6,13 +6,15 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import javax.json.JsonArray;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-//import org.json.JSONArray;
-//import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 
 /**
  * Root resource (exposed at "myresource" path)
@@ -35,11 +37,12 @@ public class MyResource {
     
     //
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
     @Path("/{para}")
-    public String getLogin(@PathParam("para") int id) {
+    public JSONArray getLogin(@PathParam("para") int id) {
     System.out.println("hey");	
     	String output="";
+    	JSONArray list=null;
     	try
     	    {
 System.out.println("INside try");
@@ -48,26 +51,30 @@ System.out.println("INside try");
     	        conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","root");
     	        ResultSet rs,rs1=null;
     	        Statement stm1=conn.createStatement();
-    	       String sql="Select StudentName FROM student WHERE StudentID="+id;
-    	       //String abc="Select StudentName FROM test WHERE StudentID="+id;
-    	        //JSONArray list = new JSONArray();
-    	        rs=stm1.executeQuery(sql);
+    	       //String sql="Select StudentName FROM student WHERE StudentID="+id;
+    	       String abc="Select * FROM student";
+    	         list = new JSONArray();
+    	        rs=stm1.executeQuery(abc);
     	        //rs1=stm1.executeQuery(abc);
     	   
     	        while(rs.next())
     	        {
-    	           /* JSONObject obj= new JSONObject();
-    	            //obj.put("no", rs.getString("id"));
+    	            JSONObject obj= new JSONObject();
+    	            obj.put("no", rs.getString("StudentID"));
     	            obj.put("name", rs.getString("StudentName"));
+    	            System.out.println(obj);
+    	            list.put(obj);
     	            //obj.put("dept", rs.getString("department"));
     	            //obj.put("addr", rs.getString("address"));
-    	            */
-    	        	 output=rs.getString("StudentName");
-    	        	System.out.println("name:"+output);
-    	           // System.out.println(obj.toString());
+    	            
+    	        	 //output=rs.getString("StudentName");
+    	        	//System.out.println("name:"+output);
+    	          // System.out.println(obj.toString());
+    	       //output=rs.getString("StudentName");  
+    	       
     	        }
-
-    	        
+                 
+    	        System.out.println(list);
     	        //JSONObject obj=new JSONObject();
     	        //obj.put("Name", rs.getString("StudentName"));
     	        
@@ -80,6 +87,6 @@ System.out.println("INside try");
     	        System.out.println(ex);
     	    }
     	
-        return output;
+        return list;
     }
 }
--- getatutor/src/main/webapp/index.jsp
+++ getatutor/src/main/webapp/index.jsp
@@ -8,24 +8,38 @@
     <div ng-app="myApp" ng-controller="myController">
     	<input type="text" ng-model="user">
     	<input type="button" ng-click="login()">
+    
+    <ul ng-repeat="data1 in records">
+				<li>{{data1.name}}</li>
+				<li>{{data1.no}}</li>
+			</ul>
+		
+    
     </div>
-</body>
+
 <script>
 	var app=angular.module('myApp',[]);
 	app.controller("myController",function($scope,$http)
 			{
+				//$scope.records = [] ; 
 				alert('in controller');
 				$scope.login=function(){
 					var url="webapi/myresource/"+$scope.user;
 					alert('in login func');
-					$http.get(url).success(function(response){
-						alert(response);	
+					$http.get(url).success(function(data){
+						alert(data);
+						console.log(JSON.stringify(data));
+						$scope.records = data; 
+						//alert($scope.records[0].name);
+					    //alert($scope.records[0].no);
 						
-					}).error(function(response){
+					}).error(function(data){
 						alert('error');	
 						
 					});
 				}
 			});
 </script>
+</body>
+
 </html>
